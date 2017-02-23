39acdce6523a37bdab560ca75f504bd5d6a4f8f0
Qaisar Nadeem <iamqaisarnadeem@gmail.com>
2017-02-23 17:47:14 +0500

    Rspec specs added for requests

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,10 +1,19 @@
-# require 'rails_helper'
-#
-# RSpec.describe "Comments", type: :request do
-#   describe "GET /comments" do
-#     it "works! (now write some real specs)" do
-#       get comments_path
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "Tickets", type: :request do
+  before(:each) do
+    @user = FactoryGirl.create :user
+    @auth_headers = @user.create_new_auth_token
+  end
+
+  it "Ticket create API should create ticket and return the ticket as response" do
+    ticket = @user.tickets.create(title: "Sample" ,  description: "Test description")
+    description = "test comment"
+    post comments_path,params: {ticket_id: ticket.id,comment: {description: description}}, headers: @auth_headers
+    json = JSON.parse(response.body)
+    expect(json["description"]).to eq(description)
+    expect(json["user"]["id"]).to eq(@user.id)
+    expect(ticket.comments&.last&.id).to eq(json['id'])
+  end
+
+end
--- spec/requests/tickets_spec.rb
+++ spec/requests/tickets_spec.rb
@@ -1,10 +1,60 @@
-# require 'rails_helper'
-#
-# RSpec.describe "Tickets", type: :request do
-#   describe "GET /tickets" do
-#     it "works! (now write some real specs)" do
-#       get tickets_path
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "Tickets", type: :request do
+  before(:each) do
+    @user = FactoryGirl.create :user
+    @auth_headers = @user.create_new_auth_token
+  end
+
+  describe "GET /tickets" do
+    it "should return 200 response after successfull login" do
+      get tickets_path,params: {}, headers: @auth_headers
+      expect(response).to have_http_status(200)
+    end
+    it "should return 401 for invalid credentials" do
+      get tickets_path,params: {}
+      expect(response).to have_http_status(401)
+    end
+
+    it "should return ticket as json response for user who has tickets" do
+      ticket = @user.tickets.create(title: "Sample" ,  description: "Test description")
+      get tickets_path,params: {}, headers: @auth_headers
+      json = JSON.parse(response.body)
+      expect(json["tickets"].first['id']).to eq(ticket.id)
+    end
+
+    it "should return valid tickets count in response" do
+      ticket = @user.tickets.create(title: "Sample" ,  description: "Test description")
+      get tickets_path,params: {}, headers: @auth_headers
+      json = JSON.parse(response.body)
+      expect(json["total"]).to eq(1)
+    end
+
+
+    it "Ticket show api should return valid response with ticket as body" do
+      ticket = @user.tickets.create(title: "Sample" ,  description: "Test description")
+      get ticket_path(ticket),params: {}, headers: @auth_headers
+      json = JSON.parse(response.body)
+      expect(json["id"]).to eq(ticket.id)
+      expect(json["title"]).to eq(ticket.title)
+      expect(json["description"]).to eq(ticket.description)
+    end
+
+    it "Ticket create API should create ticket and return the ticket as response" do
+      title = "Test"
+      description = "sample description"
+      post tickets_path,params: {ticket: {title: title , description: description}}, headers: @auth_headers
+      json = JSON.parse(response.body)
+      expect(json["title"]).to eq(title)
+      expect(json["description"]).to eq(description)
+      expect(json["user_id"]).to eq(@user.id)
+    end
+
+    it "change status ticket should change the status as provided" do
+      ticket = @user.tickets.create(title: "Sample" ,  description: "Test description")
+      put change_status_ticket_path(ticket),params: {status: "in_progress"}, headers: @auth_headers
+      json = JSON.parse(response.body)
+      expect(json["status"]).to eq("in_progress")
+    end
+  end
+end
